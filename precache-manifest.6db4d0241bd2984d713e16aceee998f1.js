948582064b150935bec77c6734ab9b26accaba0e
Moises Olvera <moises.olvera@itexico.com>
2020-08-02 22:07:48 -0500

    Updates

---
+++ precache-manifest.6db4d0241bd2984d713e16aceee998f1.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8414728617387c6e0fdac504386a8055",
+    "url": "/moyolvera.github.io/index.html"
+  },
+  {
+    "revision": "effa7ad0fc8531f76c09",
+    "url": "/moyolvera.github.io/static/css/main.e8c24495.chunk.css"
+  },
+  {
+    "revision": "ef6f2dc94eac2454727c",
+    "url": "/moyolvera.github.io/static/js/2.fdda7aab.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/moyolvera.github.io/static/js/2.fdda7aab.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "effa7ad0fc8531f76c09",
+    "url": "/moyolvera.github.io/static/js/main.4978daf4.chunk.js"
+  },
+  {
+    "revision": "31997255df4dd6bf2b73",
+    "url": "/moyolvera.github.io/static/js/runtime-main.a134a9b0.js"
+  }
+]);
