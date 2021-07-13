22fb7bbc3e8e62427a6e244a3b98f2c3139a6cb9
Moy Olvera <moi6olvera@gmail.com>
2021-07-13 14:41:07 -0500

    Sometimes simple is better

--- src/components/index.ts
+++
@@ -1,3 +0,0 @@
-import FeaturesMap from './FeaturesMap';
-
-export { FeaturesMap };
--- src/sections/index.ts
+++
@@ -1,9 +0,0 @@
-import About from "./About";
-import ContactUs from "./ContactUs";
-import Footer from "./Footer";
-import Header from "./Header";
-import Portfolio from "./Portfolio";
-import Resume from "./Resume";
-import Testimonials from "./Testimonials";
-
-export { About, ContactUs, Footer, Header, Portfolio, Resume, Testimonials };
